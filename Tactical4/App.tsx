513ed53a68b6b3ad0c9265750ee552ff28b618d6
antoinetardivel <tardivelantoine@gmail.com>
2020-12-03 16:25:44 +0100

    intégration grille

--- Tactical4/App.tsx
+++ Tactical4/App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import Puissance4 from './components/puissance4';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Puissance4></Puissance4>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#04091B',
   },
 });
